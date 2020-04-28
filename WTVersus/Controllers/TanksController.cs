0c8dc6a57e0feb0f9f41b70a547acbeb740d0af6
Bogdan <bo7145907@gmail.com>
2020-04-29 01:25:05 +0300

    Tank dev in progress

--- WTVersus/Controllers/TanksController.cs
+++ WTVersus/Controllers/TanksController.cs
@@ -26,9 +26,20 @@ namespace WTVersus.Controllers
         #endregion
 
         [HttpGet]
-        public IActionResult Compare()
+        public IActionResult Compare(int vehicle1, int vehicle2, int vehicle3, int vehicle4)
         {
-            return View();
+            var tankssFromDb = Context.Tanks.OrderBy(x => x.BR).ToList();
+            tankssFromDb.Insert(0, new Tank { Image = "http://wtversus.com/images/EmptyPlane.png", Nation = "EmptyFlag", Name = "Select vehicle", VehicleId = 0 }); //Перший пустий елемент, щоб не засмічувати БД
+            var selectedTanks = new List<Tank>();
+            ViewBag.AllTanksSelected = tankssFromDb;
+
+            selectedTanks.Add(tankssFromDb.FirstOrDefault(p => p.VehicleId == vehicle1));
+            selectedTanks.Add(tankssFromDb.FirstOrDefault(p => p.VehicleId == vehicle2));
+            selectedTanks.Add(tankssFromDb.FirstOrDefault(p => p.VehicleId == vehicle3));
+            selectedTanks.Add(tankssFromDb.FirstOrDefault(p => p.VehicleId == vehicle4));
+
+            return View(selectedTanks);
         }
+
     }
 }
