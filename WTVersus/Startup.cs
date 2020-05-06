fe4e1761ef82eacaa7d41e19f239fd43fd710b8a
Bogdan <bo7145907@gmail.com>
2020-05-07 00:20:42 +0300

    New menu

--- WTVersus/Startup.cs
+++ WTVersus/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using WTVersus.Models;
 
 namespace WTVersus
@@ -33,7 +34,7 @@ namespace WTVersus
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
